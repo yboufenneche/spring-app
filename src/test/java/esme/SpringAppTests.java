b8e01f9f480976e6f6334d00e8afbc56ef5f653c
boufenny <yassine.boufenneche@esme.fr>
2023-04-07 17:03:12 +0200

    Initial commit

---
+++ src/test/java/esme/SpringAppTests.java
@@ -0,0 +1,13 @@
+package esme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
